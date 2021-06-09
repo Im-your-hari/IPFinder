3c556a6ea93897fa4fed13f0c4f5d7238380727c
Hari <baluoger91@gmail.com>
2021-06-09 20:18:48 +0530

    Some advance in UI , Error handling...

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.core.window import Window
 import socket as s
 
 
@@ -10,24 +11,33 @@ class MyPage(BoxLayout):
     def __init__(self):
         super().__init__()
         self.orientation = 'vertical'
-        self.label = Label(text="_Enter Domain_")
-        self.textinput = TextInput(multiline=False)
-        self.button = Button(text="Find",on_press=self.find_domain)
+        self.padding = 50
+        self.spacing = 20
+        self.label = Label(text="{Mr.X}")
+        self.textinput = TextInput(multiline=False,hint_text="Enter Domain...")
+        
+        
+        self.button = Button(text="Find",background_color=(0,0,1,1),on_press=self.find_domain)
 
         self.add_widget(self.label)
         self.add_widget(self.textinput)
         self.add_widget(self.button)
     
     def find_domain(self,instance):
-        self.label.text = "IP"
+        #self.label.text = "IP"
         #self.textinput.text = "This is IP"
         a = self.textinput.text
-        self.textinput.text = s.gethostbyname(a)
+        try:
+            self.textinput.text = s.gethostbyname(a)
+        except Exception:
+            self.textinput.text = ""
+            self.textinput.hint_text = "Invalid Domain...! Please try another..."
         
 
 
 class Main(App):
     def build(self):
+        Window.clearcolor = (0,0,255,1)
         return MyPage()
         
 if __name__ == '__main__':
